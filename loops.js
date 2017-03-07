c5ee28d6891d20c8bdbfadafc1f0fa76d464ab98
Diego Schwartz <dschwa01@gmail.com>
2017-03-07 01:00:06 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,40 @@
+function forLoop (array){
+for ( var i = 0; i < 25; i++){
+	if (i===0){
+	array.push("I am " +i+" strange loop.")
+    }
+
+	else if (i === 1){
+	array.push("I am " +i+" strange loop.")
+    }
+	else{
+	 array.push("I am " +i+ " strange loops.")
+    }
+
+
+
+
+}
+return array
+
+
+}
+
+function whileLoop (n){
+		while (n>0 ){
+			console.log(--n)
+			}
+return "done"
+
+
+}
+
+
+function doWhileLoop(array){
+    do{
+      return ['array']
+    }
+    while (maybeTrue())
+
+
+}
